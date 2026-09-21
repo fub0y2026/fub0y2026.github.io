346becbbc00946d0c4756592591a3b7be3446496
fumx1314 <fuboy_2024@qq.com>
2026-09-21 21:58:43 +0800

    Site updated: 2026-09-21 21:57:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760542455676";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1789998959123";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
